aec0f3bf82aba5e1862bd39dcb414cfcc26fcaaf
vdanylo <danil.voronov@knu.ua>
2024-05-23 16:57:36 +0300

    commit

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,21 +1,27 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# """
+# WSGI config for core project.
+#
+# It exposes the WSGI callable as a module-level variable named ``application``.
+#
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+# """
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# #
+# # application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# # from vercel_wsgi import VercelWSGI
 #
 # application = get_wsgi_application()
-from django.core.wsgi import get_wsgi_application
-# from vercel_wsgi import VercelWSGI
+# # app = application#VercelWSGI(application)
 
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')  # Adjust 'core.settings' to your settings module
 application = get_wsgi_application()
-# app = application#VercelWSGI(application)
